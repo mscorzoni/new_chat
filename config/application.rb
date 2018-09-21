3fe9f3f6ae53b503171f39bcf7318060a40f468b
Marcos Scorzoni <mlessa@gmail.com>
2018-09-21 10:33:16 +0100

    webpack setup

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,12 @@ module NewChat
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+        g.test_framework false
+        g.stylesheets    false
+        g.javascripts    false
+        g.helper         false
+        g.channel        false
+    end
   end
 end
